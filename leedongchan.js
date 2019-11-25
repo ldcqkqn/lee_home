b4229b480f8865986de40e73246feccb7a08f0ea
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2019-11-26 01:06:48 +0900

    home_01

---
+++ leedongchan.js
@@ -0,0 +1,11 @@
+function navClick(navname){
+    if(navname ==='practice'){
+        document.location.href = 'https://www.naver.com/';
+    }else if(navname ==='make'){
+        document.location.href = 'https://www.nate.com/';
+    }else if(navname ==='define'){
+        document.location.href = 'https://www.hanmail.net/';
+    }else{
+        document.location.href = 'https://www.google.com/';
+    }
+}
